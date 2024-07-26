83546cf285017b04925aaa323acb7cbada5d3644
SUNGKYU KIM <seooonggyu@gmail.com>
2024-07-27 00:44:51 +0900

    aspect oriented programming -aop

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hello_spring;
 
+import hello.hello_spring.aop.TimeTraceAop;
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
 import jakarta.persistence.EntityManager;
@@ -45,5 +46,10 @@ public class SpringConfig {
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JpaMemberRepository(em);
+//    }
+
+//    @Bean
+//    public TimeTraceAop TimeTraceAop() {
+//        return new TimeTraceAop();
 //    }
 }
--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -20,15 +20,28 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    /**
+     * 회원 가입
+     */
     public Long join(Member member) {
+
+//        long start = System.currentTimeMillis();
+//
+//        try {
+//            validateDuplicateMember(member);
+//            memberRepository.save(member);
+//            return member.getId();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("join = " + timeMs + "ms");
+//        }
+
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
 
-    /**
-     * 회원 가입
-     */
     private void validateDuplicateMember(Member member) {
 //        Optional<Member> result = memberRepository.findByName(member.getName());
 //        result.ifPresent(m -> {
@@ -45,6 +58,14 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
+//        long start = System.currentTimeMillis();
+//        try {
+//            return memberRepository.findAll();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("findMembers = " + timeMs + "ms");
+//        }
         return memberRepository.findAll();
     }
 
